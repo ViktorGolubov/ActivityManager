a0465c8133af9cd4d4e4978484bee5b35011b447
Viktor Golubov <golubovviktor@mail.ru>
2019-04-03 12:21:57 +0200

    Add 'onClick' logic to several 'MainActivity' Views

--- app/src/main/java/com/example/activitymanager/MainActivity.java
+++ app/src/main/java/com/example/activitymanager/MainActivity.java
@@ -2,22 +2,33 @@ package com.example.activitymanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnLoad;
+    private TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"onCreate()",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
 
-        btnLoad = findViewById(R.id.activity_main__btn__load);
+        Button btnLoad = findViewById(R.id.activity_main__btn__load);
+        btnLoad.setOnClickListener(this);
+
+        TextView tvTitle = findViewById(R.id.activity__main__tv__title);
+        tvTitle.setOnClickListener(this);
+
+        tvName = findViewById(R.id.activity__main__tv__name);
+        tvName.setOnClickListener(this);
+
 
     }
+
 
     @Override
     protected void onStart() {
@@ -55,5 +66,28 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"onRestart()",Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onClick(View v) {
+        String message = null;
+        switch (v.getId()) {
+            case R.id.activity_main__btn__load:
+                message = "button clicked";
+                tvName.setText("Hello world!");
+                tvName.setBackgroundColor(getResources().getColor(R.color.viktorFavColor));
+                break;
+            case R.id.activity__main__tv__title:
+                message = "title clicked";
+                break;
+            case R.id.activity__main__tv__name:
+                message = "";
+                break;
+                default:
 
+        }
+
+        if (message != null) {
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
